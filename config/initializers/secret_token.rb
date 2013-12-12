c471f0afdadc5df467fbdb79feb2729859acc1a5
Ajay Vallabh <ajay.vallabh@mail.com>
2013-12-12 10:46:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MassHealth::Application.config.secret_key_base = 'ec95fd3491786e8e4e0370cfa65293a547a1425e79a88e0b1c762f1d6bfb16464c7795a7f151f558a336c2c4f28e9a03d06954bb47da7284b1e1e12f533589b1'
